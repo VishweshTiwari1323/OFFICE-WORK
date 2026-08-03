22fd198cc77d2b75a97376845dbd088a87ee0526
VishweshTiwari1323 <vishwesh1323@gmail.com>
2026-08-03 13:51:43 +0530

    Fix ALLOWED_HOSTS for Vercel

--- pm_system/settings.py
+++ pm_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4i9r($^38-xfb$6v!em+vm(*2%jt&dzihuf=t%!%@2ez(rkots
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1'] 
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
