4adda7a439db29f0b9375eeb4e4546e36e7179e6
VishweshTiwari1323 <vishwesh1323@gmail.com>
2026-08-19 19:03:09 +0530

    Show migration errors in Vercel logs for debugging

--- pm_system/pm_system/wsgi.py
+++ pm_system/pm_system/wsgi.py
@@ -7,7 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 import os
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pm_system.settings')
 
@@ -17,8 +16,8 @@ application = get_wsgi_application()
 try:
     from django.core.management import call_command
     call_command('migrate', interactive=False)
-    
     import seed_data
     seed_data.seed()
 except Exception as e:
-    print(f"Startup error: {e}")
+    import traceback
+    traceback.print_exc()
