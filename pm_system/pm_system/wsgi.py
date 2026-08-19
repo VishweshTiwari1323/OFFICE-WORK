9000924a0aa8f29651480288d73304f5955a7010
VishweshTiwari1323 <vishwesh1323@gmail.com>
2026-08-19 19:46:06 +0530

    Move vercel.json to root, fix wsgi.py paths, update ALLOWED_HOSTS

--- pm_system/pm_system/wsgi.py
+++ pm_system/pm_system/wsgi.py
@@ -1,18 +1,14 @@
-"""
-WSGI config for pm_system project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
 import os
+import sys
+from pathlib import Path
+
+# Add outer and inner pm_system directories to Python system path
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pm_system.settings')
 
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-
-# Required for Vercel to recognize the WSGI app
 app = application
